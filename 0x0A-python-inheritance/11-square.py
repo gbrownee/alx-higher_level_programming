303c361d6f3bcc3680beae0c22018e5e3ca6b96e
gbrownee <kaluberts@gmail.com>
2022-07-05 06:09:43 -0700

    redoing

--- 0x0A-python-inheritance/11-square.py
+++ 0x0A-python-inheritance/11-square.py
@@ -13,23 +13,23 @@ class Square(Rectangle):
     Square Class
     """
 
-def __init__(self, size):
-    """
-    Initializes a Square
-    """
+    def __init__(self, size):
+        """
+        Initializes a Square
+        """
 
-    self.integer_validator("size", size)
-    super().__init__(size, size)
-    self.__size = size
+        self.integer_validator("size", size)
+        super().__init__(size, size)
+        self.__size = size
 
-def __str__(self):
-    """
-    Returns a formatted string
-    """
-    return str("[Square] {}/{}".format(self.__size, self.__size))
+    def __str__(self):
+        """
+        Returns a formatted string
+        """
+        return str("[Square] {}/{}".format(self.__size, self.__size))
 
-def area(self):
-    """
-    Area function(must be implemented)
-    """
-    return self.__size ** 2
+    def area(self):
+        """
+        Area function(must be implemented)
+        """
+        return self.__size ** 2
