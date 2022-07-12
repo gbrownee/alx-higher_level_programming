9c00ef046be912c889007f54fd75d171f9f075d8
gbrownee <kaluberts@gmail.com>
2022-07-12 16:17:30 -0700

    circled

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -41,33 +41,33 @@ class Base:
         if (type(list_dictionaries) != list or
                 not all(type(x) == dict for x in list_dictionaries)):
             raise TypeError("list_dictionaries must be a list of dictionaries")
-        return json.dumps(list_dictionaries
+        return json.dumps(list_dictionaries)
 
-    @ classmethod
+    @classmethod
     def save_to_file(cls, list_objs):
         """
         Writes the JSON string representation of list_objs to a file.
         """
 
         if list_objs is None or list_objs == []:
-            jstr="[]"
+            jstr = "[]"
         else:
-            jstr=cls.to_json_string([o.to_dictionary() for o in list_objs])
-        filename=cls.__name__ + ".json"
+            jstr = cls.to_json_string([o.to_dictionary() for o in list_objs])
+        filename = cls.__name__ + ".json"
         with open(filename, 'w') as f:
             f.write(jstr)
 
-    @staticmethod:
-        def from_json_string(json_string):
-            """
-            Returns the list of the JSON string representation json_string.
-            """
+    @staticmethod
+    def from_json_string(json_string):
+        """
+        Returns the list of the JSON string representation json_string.
+        """
 
-        a=[]
+        a = []
         if json_string is not None and json_string != '':
             if type(json_string) != str:
                 raise TypeError("json_string must be a string")
-            a=json.loads(json_string)
+            a = json.loads(json_string)
         return a
 
     @classmethod
@@ -77,9 +77,9 @@ class Base:
         """
 
         if cls.__name__ == 'Rectangle':
-            dummy=cls(1, 1)
+            dummy = cls(1, 1)
         elif cls.__name__ == 'Square':
-            dummy=cls(1)
+            dummy = cls(1)
         dummy.update(**dictionary)
         return dummy
 
@@ -89,13 +89,13 @@ class Base:
         Returns a list of instances.
         """
 
-        filename=cls.__name__ + ".json"
-        a=[]
-        list_dicts=[]
+        filename = cls.__name__ + ".json"
+        a = []
+        list_dicts = []
         if os.path.exists(filename):
             with open(filename, 'r') as f:
-                s=f.read()
-                list_dicts=cls.from_json_string(s)
+                s = f.read()
+                list_dicts = cls.from_json_string(s)
                 for d in list_dicts:
                     a.append(cls.create(**d))
         return a
@@ -111,15 +111,15 @@ class Base:
                 not all(isinstance(x, cls) for x in list_objs)):
             raise TypeError("list_objs must be a list of instances")
 
-        filename=cls.__name__ + ".csv"
+        filename = cls.__name__ + ".csv"
         with open(filename, 'w') as f:
             if list_objs is not None:
-                list_objs=[x.to_dictionary() for x in list_objs]
+                list_objs = [x.to_dictionary() for x in list_objs]
                 if cls.__name__ == 'Rectangle':
-                    fields=['id', 'width', 'height', 'x', 'y']
+                    fields = ['id', 'width', 'height', 'x', 'y']
                 elif cls.__name__ == 'Square':
-                    fields=['id', 'size', 'x', 'y']
-                writer=csv.DictWriter(f, fieldnames=fields)
+                    fields = ['id', 'size', 'x', 'y']
+                writer = csv.DictWriter(f, fieldnames=fields)
                 writer.writeheader()
                 writer.writerows(list_objs)
 
@@ -129,18 +129,18 @@ class Base:
         Deserializes CSV format from a file.
         """
 
-        filename=cls.__name__ + ".csv"
-        a=[]
+        filename = cls.__name__ + ".csv"
+        a = []
         if os.path.exists(filename):
             with open(filename, 'r') as f:
-                reader=csv.reader(f, delimiter=',')
+                reader = csv.reader(f, delimiter=',')
                 if cls.__name__ == 'Rectangle':
-                    fields=['id', 'width', 'height', 'x', 'y']
+                    fields = ['id', 'width', 'height', 'x', 'y']
                 elif cls.__name__ == 'Square':
-                    fields=['id', 'size', 'x', 'y']
+                    fields = ['id', 'size', 'x', 'y']
                 for x, row in enumerate(reader):
                     if x > 0:
-                        i=cls(1, 1)
+                        i = cls(1, 1)
                         for j, e in enumerate(row):
                             if e:
                                 setattr(i, fields[j], int(e))
@@ -157,7 +157,7 @@ class Base:
         import time
         from random import randrange
 
-        t=turtle.Turtle()
+        t = turtle.Turtle()
         t.color("beige")
         turtle.bgcolor("violet")
         t.shape("square")
