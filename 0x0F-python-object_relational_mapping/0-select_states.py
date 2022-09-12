7171bb5197203e507356f1ea4e1e35d68d4d0cde
gbrownee <99354783+gbrownee@users.noreply.github.com>
2022-09-12 04:32:36 +0100

    Update 0-select_states.py

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -11,8 +11,8 @@ if __name__ == '__main__':
             host='localhost',
             port=3306,
             user=sys.argv[1],
-            passwd=sys.argv[2],
-            db=sys.argv[3])
+            password=sys.argv[2],
+            database=sys.argv[3])
 
     cursor = db.cursor()
     cursor.execute("SELECT * FROM states ORDER BY states.id ASC;")
