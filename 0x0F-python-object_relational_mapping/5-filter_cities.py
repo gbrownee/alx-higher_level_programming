63683521e86f624e23b84f702cf3ca11a96c8a36
gbrownee <99354783+gbrownee@users.noreply.github.com>
2022-09-12 04:38:42 +0100

    Update 5-filter_cities.py

--- 0x0F-python-object_relational_mapping/5-filter_cities.py
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -1,25 +1,27 @@
 #!/usr/bin/python3
-"""
-takes in the name of a state as an argument and
-lists all cities of that state,
-using the database hbtn_0e_4_usa
-"""
+""" takes name of a state as an argument and lists all cities"""
+import MySQLdb
+import sys
 
 if __name__ == '__main__':
 
-    import MySQLdb
-    import sys
+    db = MySQLdb.connect(
+        user=sys.argv[1],
+        password=sys.argv[2],
+        database=sys.argv[3],
+        host="localhost",
+        port=3306)
 
-    db = MySQLdb.connect(host='localhost', port=3306,
-                         user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
-
-    cur = db.cursor()
-    cur.execute("SELECT cities.name\
-                FROM cities LEFT JOIN states\
-                ON states.id = cities.state_id\
-                WHERE states.name = %s\
-                ORDER BY cities.id ASC", (sys.argv[4],))
-    rows = cur.fetchall()
-    print(", ".join([row[0] for row in rows]))
-    cur.close()
+    cursor = db.cursor()
+    cursor.execute("SELECT cities.name FROM cities \
+                    INNER JOIN states ON \
+                    cities.state_id=states.id \
+                    WHERE states.name=%s", (sys.argv[4],))
+    row = cursor.fetchall()
+    sp = ""
+    ar = list(r[0] for r in row)
+    for i in row:
+        sp = sp + i[0] + ", "
+    print(sp[0: -2:])
+    cursor.close()
     db.close()
